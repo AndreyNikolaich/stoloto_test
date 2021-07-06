9b113472996e29fa854177ae8dbae9e0b3c2b5f0
AndreyNikolaich <82803423+AndreyNikolaich@users.noreply.github.com>
2021-07-06 09:05:39 +0300

    создано

---
+++ pages/locators.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, '[class="iconic iconic-user"]')
+    LOGIN_EMAIL = (By.XPATH, '//*[@id="auth_login"]')
+    LOGIN_PASSWORD = (By.XPATH, '//*[@id="auth_password"]')
+    LOGIN_IN = (By.CSS_SELECTOR, '[class="pretty_button_new pretty_button type_primary btn_l scaller"]')
+    USER_NAME = (By.XPATH, '//*[@id="layout"]/div[1]/div/div[1]/div[2]/div[1]/div/a')
+    MY_TICKETS_LINK = (By.XPATH, '//*[@id="content"]/div[1]/div/div[2]/small/p/a')
+    ALL_TICKETS = (By.XPATH, '//*[@id="content"]/div[2]/div/div[2]/div/ul/li[1]/ul/li[1]/a')
+
+class WalletLocators():
+    WALLET_SUM = (By.XPATH, '//*[@id="layout"]/div[6]/div[1]/div/div[1]/div/div[1]/span[2]/span')
+    BUTTON_WALLET_REFRESH = (By.XPATH, '//*[@id="layout"]/div[6]/div[1]/div/div[1]/div/div[1]/span[3]')
+    WALLET_LINK = (By.XPATH, '//*[@id="layout"]/div[1]/div/div[1]/div[2]/div[2]/div/div[1]/a')
+
+class PageLotteryLocators7x49():
+    LINK_LOTTERY = (By.XPATH, '//*[@id="item_5150"]/a')
+    AUTO_FILL = (By.XPATH, '//*[@id="content"]/div[3]/div[1]/div[1]/div/div/div[7]/div[1]/span[1]')
+    BUTTON_PAY = (By.XPATH, '//*[text()="Оплатить из кошелька"]')
+
+class NumbersLottery():
+    NUMBER_PAGE = (By.CSS_SELECTOR, "b.game_number.selected")
+    NUMBER_TICKETS = (By.XPATH, '//*[@id="content"]/div[1]/div[1]/div/dl/dd')
+
+class UpsaleBanners():
+    BANNER_IN_PAGE_PAY = (By.XPATH, '//*[@id="content"]/div[2]/div')
---
+++ pages/main_page.py
@@ -0,0 +1,70 @@
+from .locators import MainPageLocators
+from .locators import WalletLocators
+from .locators import PageLotteryLocators7x49
+from .base_page import BasePage
+from .locators import NumbersLottery
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class AuthPage(BasePage):
+    def open_page(self):
+        link = "https://www.stoloto.ru/"
+        link.click()
+
+    def login(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        enter_email = self.browser.find_element(*MainPageLocators.LOGIN_EMAIL)
+        enter_email.send_keys(str('9998082383'))
+        enter_password = self.browser.find_element(*MainPageLocators.LOGIN_PASSWORD)
+        enter_password.send_keys(str('Test2021'))
+        login = self.browser.find_element(*MainPageLocators.LOGIN_IN)
+        login.click()
+
+    def should_login(self):
+        user_name = self.browser.find_element(*MainPageLocators.USER_NAME).text
+        assert user_name == "Andrey"
+
+    def go_to_wallet(self):
+        wallet_link = self.browser.find_element(*WalletLocators.WALLET_LINK)
+        wallet_link.click()
+
+    def should_wallet(self):
+        wallet_sum = self.browser.find_element(*WalletLocators.WALLET_SUM).text
+        assert wallet_sum >= '0'
+
+    def go_page_lottery_7x49(self):
+        page_lottery = self.browser.find_element(*PageLotteryLocators7x49.LINK_LOTTERY)
+        page_lottery.click()
+
+    def fill_ticket(self):
+        auto_fill = self.browser.find_element(*PageLotteryLocators7x49.AUTO_FILL)
+        auto_fill.click()
+
+    def go_to_pay(self):
+        button_pay_wallet = self.browser.find_element(*PageLotteryLocators7x49.BUTTON_PAY)
+        button_pay_wallet.click()
+
+    def should_button_pay(self):
+        assert self.is_element_present(By.XPATH,'//*[@id="content"]/div[5]/div/div/div[2]/div[4]/form[5]/input[2]'), "element is not present"
+
+
+    def combination_ticket_in_page(self):
+        browser = self.browser
+        number_page = []
+        for element in browser.find_element(*NumbersLottery.NUMBER_PAGE):
+            text = int(element.text)
+            number_page.append('%02d' % text)
+        return number_page
+
+    def combination_tickets_in_page_my_tickets(self):
+        element = self.browser.find_element(*NumbersLottery.NUMBER_TICKETS)
+        number_ticket = int(element.text)
+        return number_ticket
+
+    def go_to_tickets_page(self):
+        tickets_page = self.browser.find_element(*MainPageLocators.MY_TICKETS_LINK)
+        tickets_page.click()
+        all_tickets = self.browser.find_element(*MainPageLocators.ALL_TICKETS)
+        all_tickets.click()
